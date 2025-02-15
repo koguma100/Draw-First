ebfaab600a3d83f7279694b7e5207ec764954caf
koguma100 <118507004+koguma100@users.noreply.github.com>
2025-02-15 13:03:02 -0800

    StartMenu options added

--- Draw/src/StartMenu.jsx
+++ Draw/src/StartMenu.jsx
@@ -3,22 +3,116 @@ import './App.css'
 
 function StartingMenu() {
 
-  return (
-    <>
-      <h1>Draw First</h1>
-      <h2>a group drawing game</h2>
-      <div className="menu">
-        <div className="menu-option">
-            <button>Create Lobby</button>
+    const [menuData, setMenuData] = useState({
+        username: '',
+        lobbyID: '',
+        lobbyCode: '',
+        alertMessage: 'Enter Option to get started'
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setMenuData((prevData) => {
+            return {
+                ...prevData,
+                [name]: value
+            };
+        });
+    };
+
+    const handleCreateLobby = () => {
+        if (menuData.username === '') {
+            setMenuData((prevData) => {
+                return {
+                    ...prevData,
+                    alertMessage: 'Please enter a Username'
+                };
+            });
+            return;
+        }
+        else if (menuData.lobbyCode.length !== 4) {
+            setMenuData((prevData) => {
+                return {
+                    ...prevData,
+                    alertMessage: 'Please enter valid lobby code (4 characters)'
+                };
+            });
+            return;
+        }
+        else {
+            // Create a new lobby
+        }
+
+    };
+
+    const handleJoinLobby = () => {
+        if (menuData.username === '') {
+            setMenuData((prevData) => {
+                return {
+                    ...prevData,
+                    alertMessage: 'Please enter a Username'
+                };
+            });
+            return;
+        }
+        else if (menuData.lobbyID.length !== 4) {
+            setMenuData((prevData) => {
+                return {
+                    ...prevData,
+                    alertMessage: 'Please enter valid lobby ID (4 characters)'
+                };
+            });
+            return;
+        }
+        else {
+            // Join a lobby
+        }
+    };
+
+    return (
+        <>
+        <h1 className="title">Draw First</h1>
+        <h2 className="title">a group drawing game</h2>
+        <div className="menu">
+            <input 
+                type="text" 
+                name="username"
+                placeholder="Username" 
+                value={menuData.username}
+                onChange={handleChange}
+                maxLength="16"
+            />
+            <div className="menu-options">
+                <div className="menu-option">
+                    <input 
+                        type="text" 
+                        name="lobbyCode"
+                        value={menuData.lobbyCode} 
+                        onChange={handleChange} 
+                        placeholder="Lobby Code (4)" 
+                        maxLength="4"
+                    />
+                    <button onClick={handleCreateLobby}>Create Lobby</button>
+                </div>
+                <div className="menu-option">
+                    <input 
+                        type="text" 
+                        name="lobbyID"
+                        value={menuData.lobbyID} 
+                        onChange={handleChange} 
+                        placeholder="Lobby ID (4)" 
+                        maxLength="4" 
+                    />
+                    <button onClick={handleJoinLobby}>Join Lobby</button>
+                </div>
+            </div>
+
+            <div className="alert-message">{menuData.alertMessage}</div>
+            
         </div>
-        <div className="menu-option">
-            <input type="text" placeholder="Lobby ID" />
-            <button>Join Lobby</button>
-        </div>
-      </div>
-      
-    </>
-  )
+        
+        </>
+    )
 }
 
 export default StartingMenu
